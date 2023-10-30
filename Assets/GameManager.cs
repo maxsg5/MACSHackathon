ba23da4503fa180f808a1c28e9d3c8cc704d5169
Max Schafer <maxsg5@hotmail.com>
2023-10-29 23:02:52 -0600

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
@@ -13,12 +15,15 @@ public class GameManager : MonoBehaviour
     
     private int difficulty = 0; //0 = easy, 1 = medium, 2 = hard
     
-    public TMP_InputField seedInput;
+    private TMP_InputField seedInput;
+    private GameObject dropdown;
+    private TMP_Dropdown difficultyDropdown;
     //singleton
     public static GameManager _instance;
 
     private void Awake()
     {
+        seedInput = GameObject.Find("SeedInput").GetComponent<TMP_InputField>();
         if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
@@ -29,7 +34,26 @@ public class GameManager : MonoBehaviour
         //persist across scenes
         DontDestroyOnLoad(gameObject);
     }
-    
+
+    private void Start()
+    {
+        dropdown = GameObject.FindWithTag("dropdown");
+        difficultyDropdown = dropdown.GetComponent<TMP_Dropdown>();
+        difficultyDropdown.onValueChanged.AddListener(delegate { ChangeDifficulty(difficultyDropdown.value); });
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "menu")
+        {
+            dropdown = GameObject.FindWithTag("dropdown");
+            difficultyDropdown = dropdown.GetComponent<TMP_Dropdown>();
+            difficultyDropdown.onValueChanged.AddListener(delegate { ChangeDifficulty(difficultyDropdown.value); });
+            seedInput = GameObject.Find("SeedInput").GetComponent<TMP_InputField>();
+        }
+    }
+
     public int GetMazeSeed()
     {
         return mazeSeed;
